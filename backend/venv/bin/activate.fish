fd1ef82b9f697a46adcff8fb733690ffeb9fb8b0
RafaellPaivaGit <rfaellperera@gmail.com>
2026-04-26 14:54:54 -0300

    Primeiros passos agente de IA

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/marketing/Ariane/saldo_verde/backend/venv"
+set -gx VIRTUAL_ENV "/Users/rafaellpaiva/Documents/Unirv/6 Periodo/Saldo-Verde/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
